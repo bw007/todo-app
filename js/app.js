5e98e2783e63c2f13a4d46b78049736334ccc124
bw007 <mr.abdulaziz00791@gmail.com>
2023-09-30 17:50:33 +0500

    added filtering function

--- js/app.js
+++ js/app.js
@@ -7,34 +7,20 @@ const modal = document.querySelector(".modal"),
 const todoItem = document.querySelector(".todo__items-inner"),
   filterBtns = document.querySelectorAll(".todo__filter li");
 
-// FILTER ************************
-let keywords = ["all", "done", "failed"];
-filterBtns.forEach((filterBtn, i) => {
-  filterBtn.onclick = () => {
-    console.log(i);
-
-
-
-  }
-});
-
-function filter(params) {}
-// *******************************
-
 // MODAL *************************
 createBtn.onclick = () => {
   modal.classList.add("modal--active");
-}
+};
 
 modal.onclick = (e) => {
   if (e.target.classList.contains("modal--active")) {
     modal.classList.remove("modal--active");
   }
-}
+};
 
 modalClose.onclick = () => {
   modal.classList.remove("modal--active");
-}
+};
 // *******************************
 
 let items = [];
@@ -56,12 +42,32 @@ const create = (e) => {
   data.forEach((value, name) => {
     item[name] = value;
   });
-
-  items.push(item);
+  
+  items.push({...item, done: false, failed: false});
   console.log(items);
   localStorage.setItem("items", JSON.stringify(items));
   render(items, items.length - 1);
 };
+
+// FILTER ************************
+let keywords = ["all", "done", "failed"];
+filterBtns.forEach((filterBtn, i) => {
+  filterBtn.onclick = (e) => {
+    filter(e.target.dataset[keywords[i]])
+  };
+});
+
+function filter(keywrd) {
+  if (localStorage.getItem("items")) {
+    const fl = items.filter((item) => item[keywrd] === true);
+    if (keywrd == "all") {
+      render(items);
+    } else {
+      render(fl)
+    }
+  }
+}
+// *******************************
 
 // Rendering UI *****************
 function render(items, len) {
@@ -72,7 +78,10 @@ function render(items, len) {
 
     todoItem.innerHTML += `
       <section class="todo__item ${len == i ? "popup" : ""}">
-        <div class=""></div>
+        <div class="todo-item__icons">
+          <img class="todo-item__edit" src="../img/pencil.svg" />
+          <img class="todo-item__remove" src="../img/remove.svg" />
+        </div>
         <h3 class="todo-item__headline">${title}</h3>
         <p class="todo-item__text">
           ${text}
